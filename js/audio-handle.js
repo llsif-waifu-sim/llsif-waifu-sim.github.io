aca493e6d8dad8be4a94ebd5db0850214b4ce6f1
vanstorm9 <antlowhur@yahoo.com>
2016-07-10 15:31:03 -0500

    Making some progress on the snapshot

--- js/audio-handle.js
+++ js/audio-handle.js
@@ -572,34 +572,12 @@ function changeWaifu(name, index){
 		//camera_audio.volume = voiceVolume;
 		camera_audio.play();
 
+		var backgroundpath = 'images/background/background' + background.toString() + '.png';
 
+		printPhotoCanvas(backgroundpath);
 
-
-
-		
 	}
 
 
-	document.addEventListener('DOMContentLoaded',domloaded,false);
-	function domloaded(){
-	    // your code here.
-	    var canvas = document.getElementById("snapshot-canvas");
-		var ctx = canvas.getContext("2d");
-		var data = "<svg xmlns='http://www.w3.org/2000/svg' width='200' height='200'>" +
-		             "<foreignObject width='100%' height='100%'>" +
-		               "<div xmlns='http://www.w3.org/1999/xhtml' style='font-size:20px'>" +
-		                  "<table border='1'><tr><td>row 1, cell 1</td><td>row 1, cell 2</td></tr><tr><td>row 2, cell 1</td><td>row 2, cell 2</td></tr></table>" +
-		               "</div>" +
-		             "</foreignObject>" +
-		           "</svg>";
-		var DOMURL = self.URL || self.webkitURL || self;
-		var img = new Image();
-		var svg = new Blob([data], {type: "image/svg+xml;charset=utf-8"});
-		var url = DOMURL.createObjectURL(svg);
-		
-		    ctx.drawImage(img, 0, 0);
-		    DOMURL.revokeObjectURL(url);
-		img.src = url;
-	}
 
 
--- js/canvas-manage.js
+++ js/canvas-manage.js
@@ -1,18 +1,37 @@
-var canvas = document.getElementById("snapshot-canvas");
-var ctx = canvas.getContext("2d");
-var data = "<svg xmlns='http://www.w3.org/2000/svg' width='200' height='200'>" +
-	"<foreignObject width='100%' height='100%'>" +
-		"<div xmlns='http://www.w3.org/1999/xhtml' style='font-size:20px'>" +
-			"<table border='1'><tr><td>row 1, cell 1</td><td>row 1, cell 2</td></tr><tr><td>row 2, cell 1</td><td>row 2, cell 2</td></tr></table>" +
-		 "</div>" +
-	"</foreignObject>" +
-	"</svg>";
-var DOMURL = self.URL || self.webkitURL || self;
-var img = new Image();
-var svg = new Blob([data], {type: "image/svg+xml;charset=utf-8"});
-var url = DOMURL.createObjectURL(svg);
-img.onload = function() {
-	ctx.drawImage(img, 0, 0);
-	DOMURL.revokeObjectURL(url);
-};
-img.src = url;
+function printPhotoCanvas(backgroundpath){
+
+	
+    var c = document.getElementById("snapshot-canvas");
+    var ctx = c.getContext("2d");
+    var img = document.getElementById("homeScreen");
+    ctx.drawImage(img, 0, 0, 280,140);
+
+
+
+	
+	//var canvas = document.getElementById("snapshot-canvas");
+	//var ctx = canvas.getContext("2d");
+
+	var html_text = "<table border='1'><tr><td>row 1, cell 1</td><td>row 1, cell 2</td></tr><tr><td>row 2, cell 1</td><td>row 2, cell 2</td></tr></table> Hello World";
+	//var html_text = "<img src='" + backgroundpath +"'>";
+
+
+
+	var data = "<svg xmlns='http://www.w3.org/2000/svg' width='200' height='200'>" +
+		"<foreignObject width='100%' height='100%'>" +
+			"<div xmlns='http://www.w3.org/1999/xhtml' style='font-size:20px'>" +
+				html_text +
+			 "</div>" +
+		"</foreignObject>" +
+		"</svg>";
+	var DOMURL = self.URL || self.webkitURL || self;
+	var img2 = new Image();
+	var svg = new Blob([data], {type: "image/svg+xml;charset=utf-8"});
+	var url = DOMURL.createObjectURL(svg);
+	img2.onload = function() {
+		ctx.drawImage(img2, 0, 0);
+		DOMURL.revokeObjectURL(url);
+	};
+	img2.src = url;
+	
+}
