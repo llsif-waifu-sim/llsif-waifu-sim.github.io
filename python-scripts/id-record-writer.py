2e054611558488a351c293b4820a3370998adc0c
vanstorm9 <antlowhur@yahoo.com>
2016-07-13 18:32:46 -0500

    Successfully scraped data for id array

--- python-scripts/id-record-writer.py
+++ python-scripts/id-record-writer.py
@@ -2,6 +2,47 @@ import json
 import urllib
 import urllib2
 
+def allidol2path(name):
+    if name == 'Koizumi Hanayo':
+        return 'hanayo'
+    elif name == 'Hoshizora Rin':
+        return 'rin'
+    elif name == 'Nishikino Maki':
+        return 'maki'
+    elif name == 'Kousaka Honoka':
+        return 'honoka'
+    elif name == 'Sonoda Umi':
+        return 'umi'
+    elif name == 'Minami Kotori':
+        return 'kotori'
+    elif name == 'Ayase Eli':
+        return 'eli'
+    elif name == 'Yazawa Nico':
+        return 'nico'
+    elif name == 'Toujou Nozomi':
+        return 'nozomi'
+    elif name == 'Tsushima Yoshiko':
+        return 'yoshiko'
+    elif name == 'Kunikida Hanamaru':
+        return 'hanamaru'
+    elif name == 'Kurosawa Ruby':
+        return 'ruby'
+    elif name == 'Takami Chika':
+        return 'chika'
+    elif name == 'Watanabe You':
+        return 'you'
+    elif name == 'Sakurauchi Riko':
+        return 'riko'
+    elif name == 'Kurosawa Dia':
+        return 'dia'
+    elif name == 'Ohara Mari':
+        return 'mari'
+    elif name == 'Matsuura Kanan':
+        return 'kanan'
+    else:
+        return 'none'
+
+
 def idol2path(name):
     if name == 'Koizumi Hanayo':
         return 'hanayo'
@@ -23,8 +64,30 @@ def idol2path(name):
         return 'nozomi'
     else:
         return 'none'
+
+def aquors_idol2path(name):
+    if name == 'Tsushima Yoshiko':
+        return 'yoshiko'
+    elif name == 'Kunikida Hanamaru':
+        return 'hanamaru'
+    elif name == 'Kurosawa Ruby':
+        return 'ruby'
+    elif name == 'Takami Chika':
+        return 'chika'
+    elif name == 'Watanabe You':
+        return 'you'
+    elif name == 'Sakurauchi Riko':
+        return 'riko'
+    elif name == 'Kurosawa Dia':
+        return 'dia'
+    elif name == 'Ohara Mari':
+        return 'mari'
+    elif name == 'Matsuura Kanan':
+        return 'kanan'
+    else:
+        return 'none'
     
-text_file = open("records/id-list.txt", "w")
+text_file = open("../records/id-list.txt", "w")
 text_file.write('[\n')
 print '['
 for x in range (1,960):
@@ -37,17 +100,17 @@ for x in range (1,960):
     img_url = data['transparent_image']
     img_url_idol = data['transparent_idolized_image']
 
-    if img_url != None and idol2path(name) != 'none':
+    if img_url != None and allidol2path(name) != 'none':
         # [(id),(name)]
-        text_to_save =  "['" + x_str + "','" + idol2path(name) +"','no'],\n"
-        text_to_prnt =  "['" + x_str + "','" + idol2path(name) +"','no'],"
+        text_to_save =  "['" + x_str + "','" + allidol2path(name) +"','no'],\n"
+        text_to_prnt =  "['" + x_str + "','" + allidol2path(name) +"','no'],"
         text_file.write(text_to_save)
 
         print text_to_prnt
         
-    if img_url_idol != None and idol2path(name) != 'none':
-        text_to_save =  "['" + x_str + "','" + idol2path(name) +"','yes'],\n"
-        text_to_prnt =  "['" + x_str + "','" + idol2path(name) +"','yes'],"
+    if img_url_idol != None and allidol2path(name) != 'none':
+        text_to_save =  "['" + x_str + "','" + allidol2path(name) +"','yes'],\n"
+        text_to_prnt =  "['" + x_str + "','" + allidol2path(name) +"','yes'],"
         text_file.write(text_to_save)
         print text_to_prnt
         
