aa4884060ad709cad21b242c46824d2fc1256a5e
vanstorm9 <antlowhur@yahoo.com>
2016-08-15 20:29:58 -0400

    Testing something on mobile

---
+++ js/facebook-data.js
@@ -0,0 +1,24 @@
+window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '283975341969175',
+	      xfbml      : true,
+	      version    : 'v2.7'
+	    });
+	  };
+
+	  (function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/en_US/sdk.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
+
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
