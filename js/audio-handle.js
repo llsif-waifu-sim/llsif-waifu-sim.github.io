4d0e9050de03925c8e27a1babf9e2bfd9fd3812b
vanstorm9 <antlowhur@yahoo.com>
2017-01-01 01:41:19 -0500

    Adding something new

--- js/audio-handle.js
+++ js/audio-handle.js
@@ -638,7 +638,9 @@ function timeSpeech()
 
 }
 
-
+$(window).load(function() {
+	$(".loader").fadeOut("slow");
+})
 
 window.onload = function() {
 	backgroundAudio=document.getElementById("origin-music-player");
