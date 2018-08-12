9d7193598714e00b73db8622fdf73366290111d0
vanstorm9 <antlowhur@yahoo.com>
2018-08-11 21:55:39 -0400

    Some stuff

--- js/lyricsManager.js
+++ js/lyricsManager.js
@@ -1,9 +1,48 @@
-var lyricsTypeList = ["kanji","furigana","romaji","english"]
+var lyricsTypeList = ["english","kanji","furigana","romaji"];   // English has to be the first element
 
+//var enText = "";
+//var enTextSplit = null;
+
+/*
+function combineEnglishLyrics(jpText, enText){
+  var jpTextSplit = jpText.split("\n");
+  //var jpTextSplit = jpText != '' && jpText != ' ' ? jpText.split('\n') : [];
+
+  var resText = "";
+
+  var en_iter = 0;
+  var jp_iter = 0;
+  for(var i = 0; i < jpTextSplit.length; i++){
+    var sentJP = jpTextSplit[jp_iter+i];
+    var sentEN = enTextSplit[en_iter+i];
+
+  
+    if((sentJP != "" && sentJP != " ")&& (sentEN == "" || sentEN == " ")){
+      en_iter = en_iter + 1;
+      sentEN = enTextSplit[en_iter+i];
+    }
+    
+    //if(sentJP == "" || sentJP == " "){
+    //  jp_iter = jp_iter + 1;
+    //}
+    
+    //var enTmp = en_iter + i;
+    //var jpTmp = jp_iter + i;
+    //resText = resText + String(enTmp) + ":" + String(jpTmp) + "\n";
+    if(sentJP == sentEN){
+      resText = resText + sentJP + "\n >>";
+    } else {
+      resText = resText + sentJP + "\n--" + sentEN + "\n>>";
+    }
+    
+  }
+
+  return resText;
+}
+*/
 
 function assignLyrics(songName){
-  songName = songName.toLowerCase().replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s/g, '');
-  alert(songName);
+  songName = songName.toLowerCase().replace(/[.,\/#!?'"・☆♡$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s/g, '');
   for(var i = 0; i < lyricsTypeList.length;i++){
       var lyricsType = lyricsTypeList[i];
       setLyrics(songName,lyricsType);
@@ -25,7 +64,19 @@ function setLyrics(songName,lyricsType) {
       {
         var allText = rawFile.responseText;
         var elementStr = "lyric"+lyricsType+"Area"
-        document.getElementById(elementStr).innerHTML = allText.replace(/\n/g, "<br />");;
+
+        /*
+        if(lyricsType == "english"){
+          enText = allText;
+          enTextSplit = enText.split('\n');
+          //enTextSplit = enText != '' ? enText.split('\n') : [];
+        } else {
+          allText = combineEnglishLyrics(allText, enText);
+        }
+        */
+
+
+        document.getElementById(elementStr).innerHTML = allText.replace(/\n/g, "<br />");
 
       }
      }
