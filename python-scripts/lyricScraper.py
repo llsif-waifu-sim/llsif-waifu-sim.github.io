b774d0adc434fb66899fb69fad3259d2432204a9
Vanstorm9 <antlowhur@yahoo.com>
2019-07-31 12:04:48 -0400

    Adding lyrics

--- python-scripts/lyricScraper.py
+++ python-scripts/lyricScraper.py
@@ -11,9 +11,9 @@ lyricRoot = "../../distribution/llsif-waifu-lyrics"
 rootURL = 'http://love-live.wikia.com'
 
 transDict = {u'君':u'きみ',
-	u'見':u'み',
-	u'色':u'いろ',
-	u'来':u'き'}
+        u'見':u'み',
+        u'色':u'いろ',
+        u'来':u'き'}
 
 segmenter = tinysegmenter.TinySegmenter()
 kakasi = kakasi()
@@ -24,118 +24,118 @@ jpCount = 0
 siteTabList = ['rōmaji','kanji','english']
 
 def iterateSongList(urlRead='http://love-live.wikia.com/wiki/Category:Aqours_Songs'):
-	r = requests.get(urlRead).content
-	soup = BeautifulSoup(r,'lxml')
-	contDiv = soup.find("div",{"class":"mw-content-ltr"})
-	for ulTag in contDiv.findAll("ul"):
-		for liTag in ulTag.findAll("li"):
-			title = None
-			try:
-			    title = liTag.find("a").find("img")["alt"]
-			    urlRead = rootURL + liTag.find("a")['href']
-		 	except:
-			    pass
-			if title is None:
-			    try:
-			        title = liTag.find("a")['title']
-			    except:
-			        pass
-			if title is None:
-			    continue
+        r = requests.get(urlRead).content
+        soup = BeautifulSoup(r,'lxml')
+        contDiv = soup.find("div",{"class":"mw-content-ltr"})
+        for ulTag in contDiv.findAll("ul"):
+                for liTag in ulTag.findAll("li"):
+                        title = None
+                        try:
+                            title = liTag.find("a").find("img")["alt"]
+                            urlRead = rootURL + liTag.find("a")['href']
+                        except:
+                            pass
+                        if title is None:
+                            try:
+                                title = liTag.find("a")['title']
+                            except:
+                                pass
+                        if title is None:
+                            continue
 
-			urlRead = rootURL + liTag.find("a")['href']
-			#title = liTag.find("a")['title']
-			scrapeLyrics(title,urlRead)
-			
-			# We count occurrance for lyrics
-			if enCount != jpCount:
-				print '    (Lyrics misaligned)'
-				fpPath = lyricRoot + '/revision.txt'
-				fp = open(fpPath.encode('utf8'), 'a')
-				fp.write(title.encode('utf8') + '\n')
-				fp.write('JP: ' + str(jpCount) + ' - EN: ' + str(enCount)+'\n')
-				fp.write('--------------------------\n')
-				fp.close()
+                        urlRead = rootURL + liTag.find("a")['href']
+                        #title = liTag.find("a")['title']
+                        scrapeLyrics(title,urlRead)
+                        
+                        # We count occurrance for lyrics
+                        if enCount != jpCount:
+                                print('    (Lyrics misaligned)')
+                                fpPath = lyricRoot + '/revision.txt'
+                                fp = open(fpPath.encode('utf8'), 'a')
+                                fp.write(title.encode('utf8') + '\n')
+                                fp.write('JP: ' + str(jpCount) + ' - EN: ' + str(enCount)+'\n')
+                                fp.write('--------------------------\n')
+                                fp.close()
 
 
-				title = ''.join(ch for ch in title if ch.isalnum()).lower()
+                                title = ''.join(ch for ch in title if ch.isalnum()).lower()
 
-				tmpStr = '"'+title + '",\n'
-				forbidFp.write(tmpStr.encode('utf8'))
+                                tmpStr = '"'+title + '",\n'
+                                forbidFp.write(tmpStr.encode('utf8'))
 
 def scrapeLyrics(title,urlRead):
-	global enCount
-	global jpCount
+        global enCount
+        global jpCount
 
-	r = requests.get(urlRead).content
-	soup = BeautifulSoup(r,'lxml')
-	lyricListDiv = soup.findAll("div",{"class":"tabbertab"})
+        r = requests.get(urlRead).content
+        soup = BeautifulSoup(r,'lxml')
+        lyricListDiv = soup.findAll("div",{"class":"tabbertab"})
 
-	print '-----------'
-	print title
+        print('-----------')
+        print(title)
 
-	for lyricTab in lyricListDiv:
-		lyricInnerDiv = lyricTab.find("div")
-		if lyricInnerDiv and lyricTab['title'].lower().encode('utf8') in siteTabList:
-			#print '-----------'
-			lyricType = lyricTab['title'].lower().encode('utf8')
-			text = lyricInnerDiv.getText()
-			#print lyricType
-			#print text
+        for lyricTab in lyricListDiv:
+                lyricInnerDiv = lyricTab.find("div")
+                if lyricInnerDiv and lyricTab['title'].lower().encode('utf8') in siteTabList:
+                        #print '-----------'
+                        lyricType = lyricTab['title'].lower().encode('utf8')
+                        text = lyricInnerDiv.getText()
+                        #print lyricType
+                        #print text
 
-			title = title.replace(" ","")
-			title = re.sub(r'\W+', '', title).lower()
+                        title = title.replace(" ","")
+                        title = re.sub(r'\W+', '', title).lower()
 
-			if lyricType == 'english':
-				enCount = text.count("\n")	
+                        if lyricType == 'english':
+                                enCount = text.count("\n")      
 
-			if lyricType == 'rōmaji':
-				filePath = lyricRoot+'/romaji/'+title+'-romaji.txt'
-				fd = open(filePath,"w")
-			else:
-				filePath = lyricRoot+'/'+lyricType+'/'+title+'-'+lyricType+'.txt'
-				fd = open(filePath.encode('utf8'),"w")
-			fd.write(text.encode('utf8'))
-			fd.close()
+                        if lyricType == 'rōmaji':
+                                filePath = lyricRoot+'/romaji/'+title+'-romaji.txt'
+                                fd = open(filePath,"w")
+                        else:
+                                filePath = lyricRoot+'/'+lyricType+'/'+title+'-'+lyricType+'.txt'
+                                fd = open(filePath.encode('utf8'),"w")
+                        fd.write(text.encode('utf8'))
+                        fd.close()
 
 
 
-			if lyricType == 'kanji':
-				# We already wrote the kanji text, so we will write the furigana path
-				filePath = lyricRoot+'/furigana/'+title+'-furigana.txt'
-				transText = furiganaLineTrans(text)
-				jpCount = transText.count("\n")	
-				fd = open(filePath,"w")
-				fd.write(transText.encode('utf8'))
-				fd.close()
-	return
+                        if lyricType == 'kanji':
+                                # We already wrote the kanji text, so we will write the furigana path
+                                filePath = lyricRoot+'/furigana/'+title+'-furigana.txt'
+                                transText = furiganaLineTrans(text)
+                                jpCount = transText.count("\n") 
+                                fd = open(filePath,"w")
+                                fd.write(transText.encode('utf8'))
+                                fd.close()
+        return
 
 def scrapeLocalFile(path):
-	for lines in open(path,"r"):
-		lines = lines.decode('utf-8')
-		print furiganaLineTrans(lines)
+        for lines in open(path,"r"):
+                lines = lines.decode('utf-8')
+                print(furiganaLineTrans(lines))
 
 
 def furiganaLineTrans(text):
-	kakasi.setMode("J","H") 
-	kakasi.setMode("r","Hepburn") 
-	conv = kakasi.getConverter()
+        kakasi.setMode("J","H") 
+        kakasi.setMode("r","Hepburn") 
+        conv = kakasi.getConverter()
 
-	stitchStr = ''
-	for segWord in segmenter.tokenize(text):
-		result = ""
-		if transDict.has_key(segWord):
-			result = transDict[segWord]
-		else:
-			result = conv.do(segWord)
-		if segWord == result:
-			stitchStr += segWord
-		else:
-			result = result.strip().replace("\n","")
-			useStr = segWord + '  { ' + result +' }  '
-			stitchStr += useStr
+        stitchStr = ''
+        for segWord in segmenter.tokenize(text):
+                result = ""
+                if transDict.has_key(segWord):
+                        result = transDict[segWord]
+                else:
+                        result = conv.do(segWord)
+                if segWord == result:
+                        stitchStr += segWord
+                else:
+                        result = result.strip().replace("\n","")
+                        useStr = segWord + '  { ' + result +' }  '
+                        stitchStr += useStr
 
-	return stitchStr
+        return stitchStr
 
 
 fileLoc = lyricRoot + '/revision.txt'
