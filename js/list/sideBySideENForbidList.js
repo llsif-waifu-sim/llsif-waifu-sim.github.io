ba148f3beac1b75a506066c7924aa3c14c80cc9d
vanstorm9 <antlowhur@yahoo.com>
2021-06-06 12:31:05 -0400

    Fixed introduced bug in lyrics files

--- js/list/sideBySideENForbidList.js
+++ js/list/sideBySideENForbidList.js
@@ -1,2 +1,1 @@
-var forbiddenSideBySide = [
-b'"flyintothesky",\n'
+var forbiddenSideBySide = ["ketsuinohikari","analogheart","audrey","diamond","dokipipoemotion","evergreen","flyintothesky","justbelieve","kaikasengen","kakushiaji","ketsuinohikari","makeupsessionabc","mutekikyuubeliever","starlight","sweeteyes","thesecretnight","yumegakokokarahajimaruyo"];
